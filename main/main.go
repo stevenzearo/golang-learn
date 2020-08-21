e7d5614e743ff09a673262621d43b13ebcf5a1b9
steve <2226069315@qq.com>
2020-08-20 21:51:40 -0400

    procedure control statement

--- main/main.go
+++ main/main.go
@@ -18,6 +18,7 @@ func main() {
 	v1, _ := getData()
 	_, v2 := getData()
 	println(v1, v2)
+	println("-------------")
 	//	----------Data Type------------
 	var c1 = 2 + 3i
 	var c2 = 1 + 4i
@@ -27,11 +28,71 @@ func main() {
 			println(i)
 		}
 	}
+	println("-------------")
+	// ------------goto statement---------------
 	var j = 10
-	for j >= 0 {
+	for {
 		if j%3 == 0 {
 			println(j)
 		}
+		if j == 0 {
+			goto gotoHere
+		}
 		j--
+	}
+gotoHere:
+	println("done")
+
+	println("-------------")
+	// -------------break statement---------------------
+	var k = 10
+	for {
+		if k == 0 {
+			break
+		}
+		println("k:", k)
+		k--
+	}
+	var x = 10
+breakStatement:
+	for {
+		if x == 0 {
+			break breakStatement
+		}
+		println("x:", x)
+		x--
+	}
+
+	println("-------------")
+	// -------continue statement------------------
+	var y = 10
+	for {
+		if y%2 == 0 {
+			y--
+			continue
+		} else {
+			println("y:", y)
+		}
+		y--
+		if y == 0 {
+			break
+		}
+	}
+
+continueStatement:
+	for i := 1; i < 100; i++ {
+		for j := 1; j <= i; j++ {
+			if i <= 9 {
+				print(j, "*", i, " = ", i*j)
+				print("\t\t\t")
+			} else {
+				println("continuing")
+				continue continueStatement
+			}
+			if j == 10 {
+				break continueStatement
+			}
+		}
+		println()
 	}
 }
